1f21b519bb141469b28e2589752c38a6f68443cb
Hà <nguyenhaihabdh@gmail.com>
2026-05-05 23:51:50 +0700

    init project

---
+++ src/main/java/com/dacs/fashion/fashionApplication.java
@@ -0,0 +1,12 @@
+package com.dacs.FashionWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionWebApplication.class, args);
+    }
+}
